3ba8db5c741dafc063361e1320147ad2800e00df
Vandana Naik <vandanavnaik66@gmail.com>
2024-03-17 10:31:04 +0530

    propogation concept added

--- eventPropogation/propogation.js
+++ eventPropogation/propogation.js
@@ -15,3 +15,10 @@ button.addEventListener('click',(e)=>{
 alert(`clicked ${e.currentTarget.id}`)
     
 })
+
+// bubbling is default event propogation, propogation will be from child to parent, inner to outer
+
+// to change the default propgation from child to parent, and make it parent to child=>Capturing we use by adding {capture:true}
+// in capturing parent triggers first which parent is having {capture:true}
+// parent who don't have {capture:true} will behave as bubbling
+// when the child clicked all its parents triggers not child
